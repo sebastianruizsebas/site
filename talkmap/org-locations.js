ac56f3b88150b0b5e415454a255ecfba926124f8
Sebastian Ruiz <srsebastianruiz@outlook.com>
2026-02-25 17:43:00 -0500

    portfolio: talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,5 +1,10 @@
 var addressPoints = [
   [
+    "Collaborative Research Project Meeting<br />Florida State University, Department of Psychology; Tallahassee, FL, USA",
+    30.4380832,
+    -84.2809332
+  ],
+  [
     "Python for Neural Data Analysis<br />Florida State University, Department of Psychology; Tallahassee, FL, USA",
     30.4380832,
     -84.2809332
@@ -10,14 +15,9 @@ var addressPoints = [
     -84.2809332
   ],
   [
-    "Collaborative Research Project Meeting<br />Florida State University, Department of Psychology; Tallahassee, FL, USA",
-    30.4380832,
-    -84.2809332
-  ],
-  [
-    "Poster Presentation<br />Brown University; Providence, RI, USA",
-    41.8239891,
-    -71.4128343
+    "Poster Presentation<br />Carnegie Mellon University, Neuroscience Institute; Pittsburgh, PA, USA",
+    40.4406968,
+    -80.0025666
   ],
   [
     "Poster Presentation<br />Florida State University, Honors, Scholars, and Fellows House; Tallahassee, FL, USA",
@@ -25,8 +25,8 @@ var addressPoints = [
     -84.2809332
   ],
   [
-    "Poster Presentation<br />Carnegie Mellon University, Neuroscience Institute; Pittsburgh, PA, USA",
-    40.4406968,
-    -80.0025666
+    "Poster Presentation<br />Brown University; Providence, RI, USA",
+    41.8239891,
+    -71.4128343
   ]
 ];
